6aeb34203a1a2fe9628c2389e8d947de7123a54e
jiraphat-chanchai <jiraphat.chanchai@allianz.com>
2019-08-28 14:25:13 +0700

    NgRX first time!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { StoreModule } from "@ngrx/store";
 import { NgModule } from '@angular/core';
-
+import { addCoinReducer } from "./reducers/blockchain.reducer";
 import { AppRoutingModule } from './app-routing.module';
+import { ReactiveFormsModule } from "@angular/forms";
+
 import { AppComponent } from './app.component';
+import { BlockchainComponent } from './blockchain/blockchain.component';
+import { DisplayComponent } from './display/display.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlockchainComponent,
+    DisplayComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot({
+      blockchain: addCoinReducer
+    }),
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
